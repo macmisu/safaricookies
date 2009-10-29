9f4ec1085499f7796818c86be078b188aed85ea9
sweetpproductions <sweetpproductions@e1ead69e-88e1-11de-9e21-49812d2d8162>
2009-10-29 21:29:55 +0000

    1.3.1 release

---
+++ Branches/1.3.1/Constants.h
@@ -0,0 +1,26 @@
+#define BundleIdentifier								@"com.sweetpproductions.SafariCookies"
+
+#define SCCookiesPath									@"~/Library/Cookies/Cookies.plist"
+#define SCApplicationSupportFolderPath					@"~/Library/Application Support/Safari Cookies"
+#define SCFlashCookiesPath								@"~/Library/Preferences/Macromedia/Flash Player"
+#define SCFavoriteDomainsPlistFullName					@"Favorites.plist"
+
+#define SCPreferencesCookieAcceptPolicy					@"SCcookieAcceptPolicy"
+#define SCPreferencesAutomaticMode						@"SCautomaticMode"
+#define SCPreferencesRemoveNonFavoritesWhenQuitting		@"SCremoveNonFavoritesWhenQuitting"
+#define SCPreferencesRemoveFlashCookiesWhenQuitting		@"SCremoveFlashCookiesWhenQuitting"
+#define SCPreferencesRemoveGoogleAnalyticsCookies		@"SCremoveGoogleAnalyticsCookiesWhenQuitting"
+#define SCPreferencesShouldCheckForUpdates				@"SCautomaticUpdating"
+
+#define SCPreferencesTabStateKey						@"SCtabState"
+#define SCPreferencesRemoveAllAlertKey					@"SCdontShowRemoveAllAlert"
+#define SCPreferencesExportAlertKey						@"SCdontShowExportAlert"
+#define SCPreferencesImportAlertKey						@"SCdontShowImportAlert"
+#define SCPreferencesImportFromBookmarksAlertKey		@"SCdontShowImportFromBookmarksAlert"
+#define SCPreferencesImportFromTopSitesAlertKey			@"SCdontShowImportFromTopSitesAlert"
+#define SCPreferencesImportFromHistoryAlertKey			@"SCdontShowImportFromHistoryAlert"
+
+#define SafariSupportFolderPath							@"~/Library/Safari"
+#define SafariBookmarksPlistFullName					@"Bookmarks.plist"
+#define SafariTopSitesPlistFullName						@"TopSites.plist"
+#define SafariHistoryPlistFullName						@"History.plist"
