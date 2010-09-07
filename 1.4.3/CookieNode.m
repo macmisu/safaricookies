cc4a4bca299fe63efc2b6fb40c2d3ca72ff4fb0a
sweetpproductions <sweetpproductions@e1ead69e-88e1-11de-9e21-49812d2d8162>
2010-09-07 00:19:53 +0000

    - fixed an error with cookies with no suffix

---
+++ 1.4.3/CookieNode.m
@@ -0,0 +1,213 @@
+//
+//  CookieNode.m
+//  FilteringTreeController
+//
+//  Created by John Chang on 6/14/07
+//  Modified by Russell Gray September 2009
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "CookieNode.h"
+#import "Constants.h"
+#import "SCController.h"
+
+
+@implementation CookieNode
+@synthesize _prefsController, _children;
+
+
+- (id) initWithPrefsController:(SCPreferencesModule *)controller
+{
+	if ((self = [super init]))
+	{
+		_prefsController = [controller retain];
+	}
+	return self;
+}
+
+- (void) dealloc
+{
+	[_prefsController release];
+	[super dealloc];
+}
+
++ (NSString *) _siteWithDomainName:(NSString *)domain importCookies:(BOOL)importCookies
+{	
+	NSRange localFileRange = [domain rangeOfString:@"file:///"];	//ignore local urls
+	if (localFileRange.location != NSNotFound)
+		return nil;
+	
+	if ([domain characterAtIndex:1] == '^')							//ignore local file cookies
+		return nil;
+	
+	if ([domain hasPrefix:@"."]) {
+		domain = [domain substringFromIndex:1];
+	}
+	
+	if (importCookies == YES) {
+		domain = [[NSURL URLWithString:domain] host];				//a quick way to strip down to just the host
+	}
+	
+	if ([domain hasPrefix:@"www."])
+		domain = [domain substringFromIndex:4];
+	
+	NSArray *domainComponents = [domain componentsSeparatedByString:@"."];
+	
+	if ([domainComponents count] == 1) {
+		if (domain == nil || [domain isEqualToString:@""]) {
+			return nil;
+		}
+		return [domain lowercaseString];
+	}
+	
+	if ([[[domainComponents lastObject] stringByTrimmingCharactersInSet:
+		[NSCharacterSet decimalDigitCharacterSet]] length] == 0)	//return domain if it is an ip address
+		return domain;
+	
+	int secondLastObject = [domainComponents count] - 2;
+	if ([[domainComponents lastObject] length] == 3) {				//.com .org .biz etc...
+		return [NSString stringWithFormat:@"%@.%@", [domainComponents objectAtIndex:secondLastObject],
+				[domainComponents lastObject]];
+	}
+	
+	if ([domainComponents count] > 2) {
+		int thirdLastObject = [domainComponents count] - 3;			//.com.au .co.uk .co.nz etc...
+		return [NSString stringWithFormat:@"%@.%@.%@", [domainComponents objectAtIndex:thirdLastObject],
+				[domainComponents objectAtIndex:secondLastObject], [domainComponents lastObject]];
+	}
+	
+	return [domain lowercaseString];								//everything else
+}
+
+- (NSArray *) cookies
+{
+	if (_children == nil)
+	{
+		// cookies -> cookieBuckets
+		NSHTTPCookieStorage * cs = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+		NSArray * cookies = [cs cookies];
+		NSMutableDictionary * cookieBuckets = [NSMutableDictionary dictionary];
+		NSHTTPCookie * cookie;
+		for (cookie in cookies)
+		{
+			NSString * domain = [cookie domain];
+			
+			NSString * site = [[self class] _siteWithDomainName:domain importCookies:NO];
+			if (site == nil)
+				continue;
+			NSMutableArray * siteCookies = [cookieBuckets objectForKey:site];
+			if (siteCookies == nil)
+			{
+				siteCookies = [NSMutableArray array];
+				[cookieBuckets setObject:siteCookies forKey:site];
+			}
+			[siteCookies addObject:cookie];
+		}
+		
+		//get favorites from Favorites.plist
+		NSString *applicationSupportFolder = [SCApplicationSupportFolderPath stringByExpandingTildeInPath];
+		NSString *favoriteDomainsPlistPath = [applicationSupportFolder stringByAppendingPathComponent:SCFavoriteDomainsPlistFullName];
+		NSDictionary* favoritesDictionary = [NSDictionary dictionaryWithContentsOfFile:favoriteDomainsPlistPath];
+		NSMutableArray* favoriteDomains = [favoritesDictionary objectForKey:@"Domains"];
+		NSMutableSet * unusedDomainSet = [NSMutableSet setWithArray:favoriteDomains];
+
+		// cookieBuckets -> contentArray
+		_children = [NSMutableArray new];
+		for (NSString *key in cookieBuckets)
+		{
+			BOOL isFavorite = [favoriteDomains containsObject:key];
+			if (isFavorite)
+				[unusedDomainSet removeObject:key];
+				
+			NSArray * siteCookies = [cookieBuckets objectForKey:key];
+			NSMutableDictionary* dict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+				siteCookies, @"cookies",
+				key, @"domainOnly",
+				[NSString stringWithFormat:@"%@ (%d)", key, [siteCookies count]], @"domain",
+				key, @"searchDescription",
+				[NSNumber numberWithBool:isFavorite], @"isFavorite",
+				[NSNumber numberWithBool:NO], @"isLeaf",
+				nil];
+		
+			[(NSMutableArray *)_children addObject:dict];
+		}
+	
+		NSString * domain;
+		for (domain in unusedDomainSet)
+		{
+			NSMutableDictionary * dict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+				[NSArray array], @"cookies",
+				domain, @"domainOnly",
+				[NSString stringWithFormat:@"%@ (%d)", domain, 0], @"domain",
+				[NSNumber numberWithBool:YES], @"isFavorite",
+				[NSNumber numberWithBool:YES], @"isLeaf",
+				nil];
+		
+			[(NSMutableArray *)_children addObject:dict];
+		}
+	}	
+	return [_children autorelease];
+}
+
++ (NSArray *) googleAnalyticsCookies
+{
+		NSHTTPCookieStorage * cs = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+		NSArray * cookies = [cs cookies];
+		NSMutableArray * googleCookies = [NSMutableArray array];
+		NSHTTPCookie * cookie;
+	
+		for (cookie in cookies)
+		{
+			NSString * name = [cookie name];
+			
+			NSRange range = [name rangeOfString:@"__utm"];
+			if (range.location != NSNotFound)
+			{
+				[googleCookies addObject:cookie];
+			}
+		}
+	
+	return googleCookies;	
+}
+
+- (unsigned int) cookieCount
+{
+	return [[self cookies] count];
+}
+
+- (BOOL) isLeaf
+{
+	return YES;
+}
+
+@end
+
+
+@implementation NSHTTPCookie (NSTreeControllerDataSource)
+
+- (NSString *) searchDescription
+{	
+	NSMutableArray * strings = [NSMutableArray array];
+
+	NSArray * keys = [NSArray arrayWithObjects:@"domain", @"name", @"path", @"expiresDate", @"value", nil];
+	NSString * key;
+	for (key in keys)
+	{
+		NSString * string = [[self valueForKey:key] description];
+		[strings addObject:string];
+	}
+
+	return [strings componentsJoinedByString:@"\n"];
+}
+
+- (BOOL) isFavorite
+{
+	return NO;
+}
+
+- (BOOL) isLeaf
+{
+	return YES;
+}
+
+@end
---
+++ 1.4.3/README.txt
@@ -0,0 +1,19 @@
+Once compiled, you need to make sure you have the latest version of SIMBL installed:
+http://www.culater.net/software/SIMBL/SIMBL.php
+
+and place the "Safari Cookies.bundle" into the folder:
+/Library/Application Support/SIMBL/Plugins/
+
+
+1.4.3:
+---------------------------------------------------------------
+- fixed an error with cookies with no suffix
+---------------------------------------------------------------
+
+*****PackageMaker is buggy!!!!!!
+when building the installer, remember to disable "Allow Relocation" for SIMBL 0.9.7a/SIMBL Agent.app
+
+
+TODO:
+---------------------------------------------------------------
+---------------------------------------------------------------
