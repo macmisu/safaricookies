d57d57ea5feba95c9a6847c5f684b94ee8c3f6bf
sweetpproductions <sweetpproductions@e1ead69e-88e1-11de-9e21-49812d2d8162>
2010-09-13 01:02:14 +0000

    - fix for "Remove all non-favorites" button being disabled

---
+++ Branches/1.5.2/SCController.h
@@ -0,0 +1,23 @@
+//  Created by Russell Gray 2010.
+//  Copyright 2010 SweetP Productions. All rights reserved.
+
+
+@interface SCController : NSObject
+{
+}
+
++ (SCController *)sharedController;
++ (id)allocWithZone:(NSZone *)zone;
+- (id)copyWithZone:(NSZone *)zone;
+- (id)retain;
+- (unsigned)retainCount;
+- (void)release;
+- (id)autorelease;
++ (NSString *)version;
+
++ (BOOL) domains:(NSArray *)domains containsHostname:(NSString *)hostname;
++ (NSArray *) cookiesMatchingDomains:(NSArray *)matchDomains match:(BOOL)value;
++ (void) deleteCookies:(NSArray *)deleteCookies;
+
+
+@end
